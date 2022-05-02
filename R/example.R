1041f6eff368140de2ed3824b279a1fa07612298
Dewey Dunnington <dewey@fishandwhistle.net>
2022-05-02 14:39:29 -0300

    with example names

--- R/example.R
+++ R/example.R
@@ -1,13 +1,22 @@
 
 #' GeoPackage Example Files
 #'
+#' Current example files are 'nc', geometrycollection_m', 'geometrycollection_z',
+#' 'geometrycollection_zm', 'geometrycollection', 'linestring_m',
+#' 'linestring_z', 'linestring_zm', 'linestring', 'multilinestring_m',
+#' 'multilinestring_z', 'multilinestring_zm', 'multilinestring', 'multipoint_m',
+#' 'multipoint_z', 'multipoint_zm', 'multipoint', 'multipolygon_m',
+#' 'multipolygon_z', 'multipolygon_zm', 'multipolygon', 'point_m',
+#' 'point_z', 'point_zm', 'point', 'polygon_m', 'polygon_z', 'polygon_zm',
+#' and 'polygon'.
+#'
 #' @param name The example name.
 #'
 #' @return A filename or an error if the example does not exist
 #' @export
 #'
 #' @examples
-#' gpkg_example("point")
+#' gpkg_example("nc")
 #'
 gpkg_example <- function(name) {
   result <- file.path(
