0ad857670eaa9f519cf57ae1a41d30b2dfb739f5
Dewey Dunnington <dewey@fishandwhistle.net>
2022-05-02 09:47:42 -0300

    export example

--- R/example.R
+++ R/example.R
@@ -1,4 +1,14 @@
 
+#' GeoPackage Example Files
+#'
+#' @param name The example name.
+#'
+#' @return A filename or an error if the example does not exist
+#' @export
+#'
+#' @examples
+#' gpkg_example("point")
+#'
 gpkg_example <- function(name) {
   result <- file.path(
     system.file("extdata", package = "gpkg"),
